00514da72dbecf3169d8800409152e267a83c721
mburuxx <njihiamburu.3@gmail.com>
2025-02-18 21:54:20 +0300

    Get instrument by pair name

--- instruments.py
+++ instruments.py
@@ -21,5 +21,34 @@ class Instrument():
         df = cls.get_instruments_df()
         return [Instrument(x) for x in df.to_dict(orient='records')]
 
+    '''
+    dict to access instruments by pair name
+
+    instrument_dict =
+    {
+        "EUR_USD" : Instrument(),
+        "EUR_GBP" : Instrument(),
+        "..." : Instrument(),
+    }
+
+    our_instrument = instrument_dict["EUR_USD"]
+    '''
+    @classmethod
+    def get_instruments_dict(cls):
+        i_list = cls.get_instruments_list()
+        i_keys = [x.name for x in i_list]
+        return { k:v for (k,v) in zip(i_keys, i_list) }
+
+    @classmethod
+    def get_instrument_by_name(cls, pairname):
+        d = cls.get_instruments_dict()
+        if pairname in d:
+            return d[pairname]
+        else:
+            return None
+    
+
 if __name__ == "__main__":
-    print(Instrument.get_instruments_list())
+    #for k,v in Instrument.get_instruments_dict().items():
+    #    print(k,v)
+    print(Instrument.get_instrument_by_name("EUR_USD")) 
